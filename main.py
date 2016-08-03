87ec1c3ac70cdacd8b509a362dbeb0ddb2493d11
Erin Moore <emoore@pillartechnology.com>
2016-08-03 15:22:30 -0400

    Initial Commit

---
+++ main.py
@@ -0,0 +1,5 @@
+import sys
+sys.path.append('~\_________')
+from src.MinMax import _______
+
+class main():
---
+++ test/test_MinMax.py
@@ -0,0 +1,4 @@
+import unittest
+from src.MinMax import *
+
+class test_MinMax(unittest.TestCase):
