78e66dc68acbb265754d76d04ff9d8c9f34e5d6c
Erin Moore <emoore@pillartechnology.com>
2016-08-11 15:15:09 -0400

    passes test with 3 node walk

--- test/test_MinMax.py
+++ test/test_MinMax.py
@@ -1,5 +1,6 @@
 import unittest
 from mock import Mock
+from test.create_test_states import create_test_states
 
 from src.Player import Player
 from src.State import State
@@ -10,26 +11,15 @@ class test_MinMax(unittest.TestCase):
     def setUp(self):
         MinMax.create_player = self.fake_create_player
 
-        self.fake_player = Mock()
-        self.fake_player.return_possible_states= self.fake_return_possible_states
-        self.fake_stateA = Mock()
-        self.fake_stateA.get_state.return_value = 'a'
-        self.fake_stateB = Mock()
-        self.fake_stateB.get_state.return_value = 'b'
-        self.fake_stateC = Mock()
-        self.fake_stateC.get_state.return_value = 'c'
-        self.fake_stateD = Mock()
-        self.fake_stateD.get_state.return_value = 'd'
-        self.fake_stateE = Mock()
-        self.fake_stateE.get_state.return_value = 'e'
-        self.fake_stateF = Mock()
-        self.fake_stateF.get_state.return_value = 'f'
-        self.fake_stateG = Mock()
-        self.fake_stateG.get_state.return_value = 'g'
+        create_test_states(self)
 
         self.test_action_list = [
-        {'a': self.fake_stateB, 'b': self.fake_stateD, 'c': self.fake_stateF},
-        {'a': self.fake_stateC, 'b': self.fake_stateE, 'c': self.fake_stateG}
+        {'a': self.fake_stateB},
+        {'a': self.fake_stateC},
+        {'b': self.fake_stateD, 'c': self.fake_stateF},
+        {'b': self.fake_stateE, 'c': self.fake_stateG},
+        {'d': self.fake_stateH, 'e': self.fake_stateJ, 'f': self.fake_stateL, 'g': self.fake_stateN},
+        {'d': self.fake_stateI, 'e': self.fake_stateK, 'f': self.fake_stateM, 'g': self.fake_stateO}
         ]
 
         self.minmax_to_test = MinMax(self.test_action_list)
@@ -37,6 +27,8 @@ class test_MinMax(unittest.TestCase):
         self.minmax_to_test._terminal_test = self.fake_terminal_test
 
     def fake_create_player(self):
+        self.fake_player = Mock()
+        self.fake_player.return_possible_states= self.fake_return_possible_states
         return self.fake_player
 
     def fake_return_it(self, val):
@@ -44,9 +36,9 @@ class test_MinMax(unittest.TestCase):
 
     def fake_calculate_utility_value(self, state):
         state_name = state.get_state()
-        if state_name == 'd':
+        if state_name in ['i', 'l', 'n']:
             return 1
-        elif state_name in ['g','e']:
+        elif state_name in ['h','k', '']:
             return -1
         else: return 0
 
@@ -56,7 +48,7 @@ class test_MinMax(unittest.TestCase):
         return return_list
 
     def fake_terminal_test(self, state):
-        return state.get_state() in ['d', 'e', 'f', 'g']
+        return state.get_state() in ['h', 'i', 'j', 'k', 'l', 'm', 'n', 'o']
 
     def test_state_raises_correct_error(self):
         testState= State()
@@ -75,25 +67,30 @@ class test_MinMax(unittest.TestCase):
         self.assertRaises(NotImplementedError,minmax_to_test._terminal_test, 'a')
 
     def test_minmax_handles_terminal_states(self):
-        minmax_to_test = MinMax(self.test_action_list)
         expected = None
-        actual = minmax_to_test.minmax_decision(self.fake_stateG)
+        actual = self.minmax_to_test.minmax_decision(self.fake_stateO)
 
         self.assertEqual(expected, actual)
 
-    def test_minmax_finds_max_of_two_terminal_states_given_root_node(self):
+    def test_minmax_finds_max_of_two_terminal_states_given_one_node(self):
         expected = 0
-        actual = self.minmax_to_test.minmax_decision(self.fake_stateC)
+        actual = self.minmax_to_test.minmax_decision(self.fake_stateE)
 
         self.assertEqual(expected, actual)
 
         expected = 1
-        actual = self.minmax_to_test.minmax_decision(self.fake_stateB)
+        actual = self.minmax_to_test.minmax_decision(self.fake_stateD)
 
         self.assertEqual(expected, actual)
 
     def test_minmax_finds_max_with_walk_of_two_nodes(self):
         expected = -1
+        actual = self.minmax_to_test.minmax_decision(self.fake_stateB)
+
+        self.assertEqual(expected, actual)
+
+    def test_minmax_finds_max_with_walk_of_three_nodes(self):
+        expected = 1
         actual = self.minmax_to_test.minmax_decision(self.fake_stateA)
 
         self.assertEqual(expected, actual)
