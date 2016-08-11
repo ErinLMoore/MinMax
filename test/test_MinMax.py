57a2bd64a8b08d743520e15cdad07d008a48975f
Erin Moore <emoore@pillartechnology.com>
2016-08-11 14:23:13 -0400

    returns correctly on walk with two nodes

--- test/test_MinMax.py
+++ test/test_MinMax.py
@@ -10,10 +10,8 @@ class test_MinMax(unittest.TestCase):
     def setUp(self):
         MinMax.create_player = self.fake_create_player
 
-        self.fake_player1 = Mock()
-        self.fake_player1.return_possible_states= self.fake_return_possible_states
-        self.fake_player2 = Mock()
-        self.fake_player1.return_possible_states = self.fake_return_possible_states
+        self.fake_player = Mock()
+        self.fake_player.return_possible_states= self.fake_return_possible_states
         self.fake_stateA = Mock()
         self.fake_stateA.get_state.return_value = 'a'
         self.fake_stateB = Mock()
@@ -39,7 +37,7 @@ class test_MinMax(unittest.TestCase):
         self.minmax_to_test._terminal_test = self.fake_terminal_test
 
     def fake_create_player(self):
-        return self.fake_player1
+        return self.fake_player
 
     def fake_return_it(self, val):
         return val
@@ -91,5 +89,11 @@ class test_MinMax(unittest.TestCase):
 
         expected = 1
         actual = self.minmax_to_test.minmax_decision(self.fake_stateB)
+
+        self.assertEqual(expected, actual)
+
+    def test_minmax_finds_max_with_walk_of_two_nodes(self):
+        expected = -1
+        actual = self.minmax_to_test.minmax_decision(self.fake_stateA)
 
         self.assertEqual(expected, actual)
